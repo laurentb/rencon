425e43731db68ca0235db57ed595d4db39c40724
Laurent Bachelier <laurent@bachelier.name>
2012-04-02 15:01:52 +0200

    Add working simple rename command
    * pretend mode
    * can change the mask

---
+++ rencon.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from string import Template
+import argparse
+import hashlib
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Rename files based on content.')
+
+    parser.add_argument('files', metavar='FILE', type=str, nargs='+',
+                       help='Files to rename')
+    parser.add_argument('-m', '--mask', nargs='?',
+                       help='File destination mask', default='${hash}.${ext}')
+    parser.add_argument('-p', '--pretend', action='store_true',
+                       help='Do not rename, just print')
+
+    args = parser.parse_args()
+    print 'Renaming with mask: %s' % args.mask
+    mask = Template(args.mask)
+    for f in args.files:
+        if not os.path.exists(f):
+            print >>sys.stderr, 'File %s does not exists.' % f
+        else:
+            with open(f) as fp:
+                h = hashlib.sha1(fp.read()).hexdigest()
+            ext = os.path.splitext(f)[1][1:]
+            name = mask.substitute(hash=h, ext=ext)
+            dest = os.path.join(os.path.dirname(f), name)
+            print "`%s' -> `%s'" % (f, dest)
+            if os.path.exists(dest):
+                print >>sys.stderr, 'Destination %s already exists.' % dest
+            elif not args.pretend:
+                os.rename(f, dest)
